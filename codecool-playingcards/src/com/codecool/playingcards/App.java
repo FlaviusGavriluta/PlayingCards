71a96652502213f745291772780e4f5009c34a0d
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-05-16 00:17:42 +0300

    overriding methods

--- codecool-playingcards/src/com/codecool/playingcards/App.java
+++ codecool-playingcards/src/com/codecool/playingcards/App.java
@@ -11,8 +11,12 @@ public class App {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Card card = new Card("Ace", Suit.SPADES);
-        out.println(card.getTitle());
+        Card card1 = new Card("Ace", Suit.SPADES);
+        Card card2 = new Card("Ace", Suit.SPADES);
+        out.println(card1.equals(card2));
+
+        out.println(card1);
+        out.println(card2);
         scanner.nextLine();
     }
 }
--- codecool-playingcards/src/com/codecool/playingcards/model/Card.java
+++ codecool-playingcards/src/com/codecool/playingcards/model/Card.java
@@ -1,5 +1,7 @@
 package com.codecool.playingcards.model;
 
+import java.util.Objects;
+
 public class Card {
 
     private final String symbol;
@@ -10,15 +12,7 @@ public class Card {
     public Card(String symbol, Suit suit) {
         this.symbol = symbol;
         this.suit = suit;
-        title = createTitle();
-    }
-
-    public String createTitle(){
-        return this.symbol + " of " + this.suit;
-    }
-
-    public String getTitle(){
-        return title;
+        title = this.symbol + " of " + this.suit;
     }
 
     public String getSymbol() {
@@ -27,5 +21,23 @@ public class Card {
 
     public Suit getSuit() {
         return suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return Objects.equals(symbol, card.symbol) && suit == card.suit && Objects.equals(title, card.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(symbol, suit);
+    }
+
+    @Override
+    public String toString() {
+        return title;
     }
 }
